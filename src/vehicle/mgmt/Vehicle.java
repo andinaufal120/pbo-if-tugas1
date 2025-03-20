ede34d4985f9ae24579b855a0ff552da193caf49
andinaufal120 <andinaufalnurfadhil120@gmail.com>
2025-03-20 22:10:55 +0800

    Adds getter and some descriptions

--- src/vehicle/mgmt/Vehicle.java
+++ src/vehicle/mgmt/Vehicle.java
@@ -6,12 +6,24 @@ public class Vehicle {
     private int speed;
     private double fuelLevel;
 
-    // Constructor
+    // Constructor: initialize each fields.
     public Vehicle(String name, int speed, double fuelLevel) {
         this.name = name;
         this.speed = speed;
         this.fuelLevel = fuelLevel;
     }
 
+    // Getter: will return name, speed, fuelLevel to caller.
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
 
 }
