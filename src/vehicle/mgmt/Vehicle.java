e7fe828d55362a566502bc0b54ab8e6f6203c411
andinaufal120 <andinaufalnurfadhil120@gmail.com>
2025-03-20 22:05:13 +0800

    Adds constructor and comments

--- src/vehicle/mgmt/Vehicle.java
+++ src/vehicle/mgmt/Vehicle.java
@@ -1,7 +1,15 @@
 package vehicle.mgmt;
 
 public class Vehicle {
+    // Fields
     private String name;
     private int speed;
     private double fuelLevel;
+
+    // Constructor
+    public  Vehicle(String name, int speed, double fuelLevel) {
+        this.name = name;
+        this.speed = speed;
+        this.fuelLevel = fuelLevel;
+    }
 }
