60065728c5d7a06f20d867b0106c2e96c4b81822
andinaufal120 <andinaufalnurfadhil120@gmail.com>
2025-03-22 19:07:25 +0800

    Adds new Vehicle objects.

--- src/vehicle/mgmt/VehicleDemo.java
+++ src/vehicle/mgmt/VehicleDemo.java
@@ -2,6 +2,12 @@ package vehicle.mgmt;
 
 public class VehicleDemo {
     public static void main(String[] args) {
-
+        Vehicle[]  vehicles = {
+                new LandVehicle("LCGC", 60, 40, 4),
+                new WaterVehicle("Speedboat", 70, 100, true),
+                new LandVehicle("Mobil Intel", 240, 88, 4),
+                new LandVehicle("Bemo", 30, 30, 3),
+                new WaterVehicle("Sailor Boat", 10, 0, false)
+        };
     }
 }
