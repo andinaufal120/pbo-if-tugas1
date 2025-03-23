f8cff49050a25eb90e08492914f899e7cd3b9309
andinaufal120 <andinaufalnurfadhil120@gmail.com>
2025-03-23 11:46:03 +0800

    Fix newFuelLevel calculation bug.

--- src/vehicle/mgmt/WaterVehicle.java
+++ src/vehicle/mgmt/WaterVehicle.java
@@ -18,7 +18,7 @@ public class WaterVehicle extends Vehicle {
     @Override
     public void calculateFuelConsumption(double distance) {
         double fuelConsumption = distance / 8;
-        double newFuelLevel = getFuelLevel() + fuelConsumption;
+        double newFuelLevel = getFuelLevel() - fuelConsumption;
         setFuelLevel(newFuelLevel);
 
         System.out.println("Fuel consumed for " + distance + " km: " + fuelConsumption + ". Remaining fuel level: " + getFuelLevel());
